d5bb1af8432ec9043f989a0ae2582efe941d71a3
fatanar <128837367+fatanar@users.noreply.github.com>
2024-08-14 14:08:54 +0700

    benerin route di front end (#6)
    * feat: baru role dalam register dan login
    
    * feat: register role
    
    * feat: register role tambah folder login
    
    * feat: register role tambah folder login
    
    * feat: login
    
    * feat: login register tinggal rapihin aja udh succes
    
    * feat: sudah diedit menjadi lebih baik
    
    * feat: edit output sama benerin next link

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,20 +1,12 @@
 import LoginPage from '@/features/login'
 import React from 'react'
 
-<<<<<<< HEAD
 const Login = () => {
-=======
-const Register = () => {
->>>>>>> main
   return (
     <div>
-        <LoginPage/>
+      <LoginPage />
     </div>
   )
 }
 
-<<<<<<< HEAD
 export default Login
-=======
-export default Register
->>>>>>> main
